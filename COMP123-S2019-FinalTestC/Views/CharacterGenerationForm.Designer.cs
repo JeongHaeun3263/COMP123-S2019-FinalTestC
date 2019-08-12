ffc342df9845d27f8f6e0b01332576c02752212d
HAEUN JEONG <jhe3263@gmail.com>
2019-08-12 15:30:11 -0400

    navigator for tab

--- COMP123-S2019-FinalTestC/Views/CharacterGenerationForm.Designer.cs
+++ COMP123-S2019-FinalTestC/Views/CharacterGenerationForm.Designer.cs
@@ -28,9 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.IdentityPage = new System.Windows.Forms.TabPage();
+            this.AbilityPage = new System.Windows.Forms.TabPage();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.SkillsPage = new System.Windows.Forms.TabPage();
+            this.ChracterSheetPage = new System.Windows.Forms.TabPage();
+            this.MainTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainTabControl
+            // 
+            this.MainTabControl.Controls.Add(this.IdentityPage);
+            this.MainTabControl.Controls.Add(this.AbilityPage);
+            this.MainTabControl.Controls.Add(this.SkillsPage);
+            this.MainTabControl.Controls.Add(this.ChracterSheetPage);
+            this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainTabControl.Location = new System.Drawing.Point(0, 0);
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(782, 447);
+            this.MainTabControl.TabIndex = 0;
+            // 
+            // IdentityPage
+            // 
+            this.IdentityPage.Location = new System.Drawing.Point(4, 33);
+            this.IdentityPage.Name = "IdentityPage";
+            this.IdentityPage.Padding = new System.Windows.Forms.Padding(3);
+            this.IdentityPage.Size = new System.Drawing.Size(774, 410);
+            this.IdentityPage.TabIndex = 0;
+            this.IdentityPage.Text = "Identity";
+            this.IdentityPage.UseVisualStyleBackColor = true;
+            // 
+            // AbilityPage
+            // 
+            this.AbilityPage.Location = new System.Drawing.Point(4, 33);
+            this.AbilityPage.Name = "AbilityPage";
+            this.AbilityPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AbilityPage.Size = new System.Drawing.Size(774, 410);
+            this.AbilityPage.TabIndex = 1;
+            this.AbilityPage.Text = "Abilities";
+            this.AbilityPage.UseVisualStyleBackColor = true;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(634, 469);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(136, 63);
+            this.NextButton.TabIndex = 1;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(12, 469);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(136, 63);
+            this.BackButton.TabIndex = 1;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // SkillsPage
+            // 
+            this.SkillsPage.Location = new System.Drawing.Point(4, 33);
+            this.SkillsPage.Name = "SkillsPage";
+            this.SkillsPage.Size = new System.Drawing.Size(774, 410);
+            this.SkillsPage.TabIndex = 2;
+            this.SkillsPage.Text = "Skills";
+            this.SkillsPage.UseVisualStyleBackColor = true;
+            // 
+            // ChracterSheetPage
+            // 
+            this.ChracterSheetPage.Location = new System.Drawing.Point(4, 33);
+            this.ChracterSheetPage.Name = "ChracterSheetPage";
+            this.ChracterSheetPage.Size = new System.Drawing.Size(774, 410);
+            this.ChracterSheetPage.TabIndex = 3;
+            this.ChracterSheetPage.Text = "Character Sheet";
+            this.ChracterSheetPage.UseVisualStyleBackColor = true;
+            // 
+            // CharacterGenerationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.MainTabControl);
+            this.Name = "CharacterGenerationForm";
+            this.Text = "Character Gererator";
+            this.MainTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl MainTabControl;
+        private System.Windows.Forms.TabPage IdentityPage;
+        private System.Windows.Forms.TabPage AbilityPage;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.TabPage SkillsPage;
+        private System.Windows.Forms.TabPage ChracterSheetPage;
     }
 }
