3ddc5696ac18d76781c9b0c2fda4d4a25d5bb29d
HAEUN JEONG <jhe3263@gmail.com>
2019-08-12 14:53:14 -0400

    Added several classes to the project

---
+++ COMP123-S2019-FinalTestC/Objects/Identity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * STUDENT NAME: Haeun Jeong
+ * STUDENT ID: 301004579
+ * DESCRIPTION: This is the Identity Class to be used with the CaracterPortfolio class
+ */
+
+namespace COMP123_S2019_FinalTestC.Objects
+{
+    public class Identity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ COMP123-S2019-FinalTestC/Objects/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * STUDENT NAME: Haeun Jeong
+ * STUDENT ID: 301004579
+ * DESCRIPTION: This is the Skill class to be used with 
+ */
+
+namespace COMP123_S2019_FinalTestC.Objects
+{
+    public class Skill
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
